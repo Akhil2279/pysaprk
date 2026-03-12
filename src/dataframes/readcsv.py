831b1a35de5719e887af67c7db8ddeffe18beba8
Akhil2279 <akhilakhi2528@gmail.com>
2026-03-12 18:56:46 +0530

    updating dataframe file

--- src/dataframes/readcsv.py
+++ src/dataframes/readcsv.py
@@ -1,35 +1,39 @@
+from Tools.scripts.generate_opcode_h import header
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
 spark = SparkSession.builder.appName("Read CSV").getOrCreate()
 
-data = [
-    ("Akhil", 25, "India"),
-    ("Ravi", 30, "USA"),
-    ("John", 28, "UK")
-]
+emp_df = spark.read.option("header", True).option("inferSchema" , True).csv("C:/Users/akhil/OneDrive/Documents/employees.csv")
 
-columns = ["name", "age", "country"]
+emp_df.show()
 
-df = spark.createDataFrame(data, columns)
+emp_df.printSchema()
 
-df.show()
 
-input_schema = StructType([
-    StructField("emp_id",IntegerType(),True),
-    StructField("emp_name",StructType(), True),
-    StructField("salary", IntegerType(), True),
-    StructField("Country", StringType(), True)
-])
 
-#df.write.option("header", True).csv("C:/Users/akhil/OneDrive/Desktop/pyspark/src/dataframes/output.csv")
 
-df = spark.read\
-    .option("header",True)\
-    .option("delimiter", "|")\
-    .option("inferSchema", True)\
-    .schema(input_schema)\
-    .csv("C:/Users/akhil/OneDrive/Desktop/pyspark/src/dataframes/output.csv")
+
+
+
+
+
+
+# input_schema = StructType([
+#     StructField("emp_id",IntegerType(),True),
+#     StructField("emp_name",StructType(), True),
+#     StructField("salary", IntegerType(), True),
+#     StructField("Country", StringType(), True)
+# ])
+#
+# #df.write.option("header", True).csv("C:/Users/akhil/OneDrive/Desktop/pyspark/src/dataframes/output.csv")
+#
+# df = spark.read\
+#     .option("header",True)\
+#     .option("delimiter", "|")\
+#     .option("inferSchema", True)\
+#     .schema(input_schema)\
+#     .csv("C:/Users/akhil/OneDrive/Desktop/pyspark/src/dataframes/output.csv")
 
 # Features:
 # 1. Header ("header": True)

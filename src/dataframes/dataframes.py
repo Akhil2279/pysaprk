d04480afb8987c841788a06fba5afac158f97ab9
Akhil2279 <akhilakhi2528@gmail.com>
2026-03-17 18:55:58 +0530

    updating dataframes

--- src/dataframes/dataframes.py
+++ src/dataframes/dataframes.py
@@ -2,38 +2,75 @@
 #Filter High Salary
 
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
 spark = SparkSession.builder.appName("FilterSalary").getOrCreate()
 
-df = spark.read.csv("datasets/employees.csv", header=True, inferSchema=True)
+# df = spark.read.csv("datasets/employees.csv", header=True, inferSchema=True)
+#
+# result = df.filter(df.salary > 60000)
+#
+# result.show()
+#
+#
+# #Average Salary
+#
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import avg
+#
+# spark = SparkSession.builder.appName("AverageSalary").getOrCreate()
+#
+# df = spark.read.csv("datasets/employees.csv", header=True, inferSchema=True)
+#
+# result = df.agg(avg("salary").alias("average_salary"))
+#
+# result.show()
+#
+# #Salary by Department
+#
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import sum
+#
+# spark = SparkSession.builder.appName("DepartmentSalary").getOrCreate()
+#
+# df = spark.read.csv("datasets/employees.csv", header=True, inferSchema=True)
+#
+# result = df.groupBy("department").agg(sum("salary").alias("total_salary"))
 
-result = df.filter(df.salary > 60000)
-
-result.show()
+#result.show()
 
 
-#Average Salary
 
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg
+# Create Data Frame
 
-spark = SparkSession.builder.appName("AverageSalary").getOrCreate()
+schema = StructType([
+    StructField ( "emp_id" , IntegerType() , True),
+    StructField("name" , StringType() , True),
+    StructField("dep" , StringType() , True),
+    StructField("Sal" , IntegerType() , True)
+])
 
-df = spark.read.csv("datasets/employees.csv", header=True, inferSchema=True)
+data = [
+    (1 , "Akhil" , "IT" , 60000),
+    (2 , "Anil" , "Sales" , 50000),
+    (3 , "Stalin" , "Medical" , 70000),
+    (4, "Ravi" , "Medical" , 40000),
+    (5, "Bhanu" , "Hr" , 55000)
+]
 
-result = df.agg(avg("salary").alias("average_salary"))
+df = spark.createDataFrame(data , schema)
+#
+# df.show()
 
-result.show()
 
-#Salary by Department
+# Write data frame to csv
 
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import sum
+#df.write.format("CSV").option("header" , True).mode("overwrite").save("location")
 
-spark = SparkSession.builder.appName("DepartmentSalary").getOrCreate()
+# Write data frame with parquet
 
-df = spark.read.csv("datasets/employees.csv", header=True, inferSchema=True)
+#df.write.format("parquet").mode("overwrite").save("location")
 
-result = df.groupBy("department").agg(sum("salary").alias("total_salary"))
+#write dataframe to json with schema
 
-result.show()
+df.write.format("json").mode("overwrite").save("location")
